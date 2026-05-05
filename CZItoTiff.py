8c6d3e06fa64f2459a987d18ab315400a3399951
justbonaventura <justinabonaventura@gmail.com>
2026-05-05 16:55:02 -0600

    CZItoTiff fixes

--- CZItoTiff.py
+++ CZItoTiff.py
@@ -9,19 +9,19 @@ import math
 import ast
 
 #Now to add pointing so we can go from rabbit number and block to all of this-
-rab_ID='R23-292'
-block_no='block07'
+rab_ID='R24-058'
+block_no=4
 
 # #Path to Savepoint-
-BaseSavePath="/Users/jbonaventura/Downloads/"
-save_dirpath=os.path.join(BaseSavePath, rab_ID, "HnE", block_no)
+BaseSavePath="/System/Volumes/Data/ceph/hifu/users/jbonaventura/RabbitRegistrationProj/RabbitData"
+save_dirpath=os.path.join(BaseSavePath, rab_ID, "HnE", "Block0"+str(block_no))
 if not os.path.exists(save_dirpath):
     os.makedirs(save_dirpath)
 
 #With this we can do-
 BaseCephPath="/System/Volumes/Data/ceph/hifu/animal_data/IACUC1800/"
 #Path to CSV-
-csv_dirpath=os.path.join(BaseCephPath, rab_ID, rab_ID + "_BlockFaceImages",block_no,"csv_files")
+csv_dirpath=os.path.join(BaseCephPath, rab_ID, rab_ID + "_BlockFaceImages","block0" +str(block_no),"csv_files")
 files = [f for f in os.listdir(csv_dirpath) if f.endswith('csv')]
 sorted_filenames = sorted(files, key=lambda x: float(re.findall(r'\d+', x)[0]))
 csv_file=sorted_filenames[-1]
@@ -39,10 +39,10 @@ for i in range(csv_array.shape[0]):
 match_array = np.array(match_vals)
 
 #Path to CZI directory-
-czi_dirpath=os.path.join(BaseCephPath, rab_ID, rab_ID + "_HnE_5x", block_no)
+czi_dirpath=os.path.join(BaseCephPath, rab_ID, rab_ID + "_HnE_5x", "block0"+str(block_no))
 for file in os.listdir(czi_dirpath):
-    #Can edit for funky naming stuff that may go on-
-    if "HnE_R23-292" in file:
+    #Can edit this string for funky naming stuff that may go on or files saved in the wrong place-
+    if "_" in file:
         print(file)
         full_path = os.path.join(czi_dirpath, file)
         file_name = os.path.splitext(os.path.basename(full_path))[0]
@@ -62,12 +62,14 @@ for file in os.listdir(czi_dirpath):
 
         bbox = czifile.get_mosaic_bounding_box()
         czi_img = czifile.read_mosaic(C=0, scale_factor=1/20, region=(bbox.x, bbox.y, bbox.w, bbox.h), background_color=(1,1,1))[0,:,:,:]
+        #Can comment this out if you dont want to look at them all->
         plt.imshow(czi_img)
         plt.show()
 
         #Save Image as Tiff-
-        # new_file_path = os.path.join(save_dirpath, "HnE_IMG_"+ image_tag + ".tif")
-        # CImage = Image.fromarray(czi_img)
-        # CImage.save(new_file_path, 'TIFF')
+        new_file_path = os.path.join(save_dirpath, "HnE_IMG_"+ image_tag + ".tif")
+        print("Tiff saved to ", new_file_path)
+        CImage = Image.fromarray(czi_img)
+        CImage.save(new_file_path, 'TIFF')
 
 
